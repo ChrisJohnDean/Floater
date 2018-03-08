1c4efc2a8f256b1321f611ef95d12726e785fb4e
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-03-07 20:34:25 -0800

    Added search View Controller with UIPickerView for Floater type choices

---
+++ Floater/SearchViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SearchViewController.swift
+//  Floater
+//
+//  Created by Chris Dean on 2018-03-07.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+   
+    @IBOutlet weak var floaterTypePicker: UIPickerView!
+    var pickerData = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Connect data:
+        self.floaterTypePicker.delegate = self
+        self.floaterTypePicker.delegate = self
+        
+        // Input options into UIPickerView
+        self.pickerData = ["3D Animated", "Still", "2D Animated", "Gif"]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
