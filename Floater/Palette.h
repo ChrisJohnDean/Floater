f629f49db036417ecef74a276d5f7470effc5403
tysonparks <typarks@gmail.com>
2018-03-08 17:08:31 -0800

    Created Palette class

---
+++ Floater/Palette.h
@@ -0,0 +1,13 @@
+//
+//  Palette.h
+//  Floater
+//
+//  Created by Tyson Parks on 3/8/18.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Palette : NSObject
+
+@end
