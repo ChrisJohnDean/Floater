d7eba15112791f9683b6012cfb6b9bb7f85d657e
tysonparks <typarks@gmail.com>
2018-03-08 16:50:53 -0800

    Created Canvas class

---
+++ Floater/Canvas.m
@@ -0,0 +1,13 @@
+//
+//  Canvas.m
+//  Floater
+//
+//  Created by Tyson Parks on 3/8/18.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import "Canvas.h"
+
+@implementation Canvas
+
+@end
