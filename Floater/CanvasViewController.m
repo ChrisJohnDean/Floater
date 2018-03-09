73e9aaef78aaebe755c4295eb3668d14a607d4eb
tysonparks <typarks@gmail.com>
2018-03-09 14:19:34 -0800

    Successfully implemented trigger for drop into canvas

--- Floater/CanvasViewController.m
+++ Floater/CanvasViewController.m
@@ -11,10 +11,11 @@
 
 @interface CanvasViewController () <UIGestureRecognizerDelegate>
 
-@property (nonatomic, weak) UIView *canvasView;
+@property (nonatomic, strong) UIView *canvasView;
 
 @property (nonatomic, strong) NSArray<FloaterObject *> *floatrsArray;
-@property (nonatomic, strong) NSMutableArray<UIView *> *floatrViewsArray;
+@property (nonatomic, strong) NSMutableArray<UIView *> *palletteFloatrViewsArray;
+@property (nonatomic, strong) NSMutableArray<UIView *> *canvasFloatrViewsArray;
 
 -(void)setupPaletteView;
 
@@ -25,7 +26,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.floatrViewsArray = [NSMutableArray new];
+    self.palletteFloatrViewsArray = [NSMutableArray new];
+    self.canvasFloatrViewsArray = [NSMutableArray new];
     self.floatrsArray = @[@1,@2,@3,@4];
     
     [self setupView];
@@ -41,16 +43,16 @@
 }
 
 -(void)setupCanvasView {
-    UIView *canvasView = [[UIView alloc] init];
-    canvasView.translatesAutoresizingMaskIntoConstraints = NO;
-    canvasView.backgroundColor = [UIColor greenColor];
-    canvasView.layer.cornerRadius = 10.0;
-    [self.view addSubview:canvasView];
+    self.canvasView = [[UIView alloc] initWithFrame:CGRectZero];
+    self.canvasView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.canvasView.backgroundColor = [UIColor greenColor];
+    self.canvasView.layer.cornerRadius = 10.0;
+    [self.view addSubview:self.canvasView];
     
-    [canvasView.widthAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.widthAnchor].active = YES;
-    [canvasView.heightAnchor constraintEqualToAnchor:canvasView.widthAnchor].active = YES;
-    [canvasView.centerXAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.centerXAnchor].active = YES;
-    [canvasView.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:20.0].active = YES;
+    [self.canvasView.widthAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.widthAnchor].active = YES;
+    [self.canvasView.heightAnchor constraintEqualToAnchor:self.canvasView.widthAnchor].active = YES;
+    [self.canvasView.centerXAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.centerXAnchor].active = YES;
+    [self.canvasView.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:20.0].active = YES;
 }
 
 -(void)setupPaletteView {
@@ -67,7 +69,7 @@
         UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(panGestureRecognized:)];
         [floatrView addGestureRecognizer:panRecognizer];
         
-        [self.floatrViewsArray addObject:floatrView];
+        [self.palletteFloatrViewsArray addObject:floatrView];
         
         CGFloat floatrWidth = 55.0;
         CGFloat floatrCount = 5;
@@ -84,20 +86,29 @@
 
 
 -(void)panGestureRecognized: (UIPanGestureRecognizer*) recognizer {
-    NSLog(@"Being touched!");
+//    NSLog(@"Being touched!");
     
     CGPoint floatrLocation = [recognizer locationInView:self.view];
     recognizer.view.center = floatrLocation;
     
-//    switch (recognizer.state) {
-//        case UIGestureRecognizerStateEnded:
-//            <#statements#>
+    switch (recognizer.state) {
+        case UIGestureRecognizerStateEnded:
+            
+            if (CGRectContainsPoint(self.canvasView.frame, floatrLocation)) {
+                NSLog(@"Hey, I'm in the canvas!");
+                // Create corresponding floatrView and add it to the canvasView
+                
+                // Option: Make pallette floatr disappear from main view (means floatr can only be used once)
+                // or instantiate new floatrView at floatr origin (means multiple versions of same floatr can be used)
+                [UIView animateWithDuration:2.0 animations:^{recognizer.view.layer.opacity = 0.0;}];
+                
+            }
     
-//            break;
-//            
-//        default:
-//            break;
-//    }
+            break;
+            
+        default:
+            break;
+    }
 }
 
 
